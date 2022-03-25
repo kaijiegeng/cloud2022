651cef23397cc1c5742098602b407c75c57611b8
kaijiegeng <137423445@qq.com>
2022-03-25 23:00:06 +0800

    config 测试地址：http://cloud-config-center.com:3344/main/config-prod.yml

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -39,14 +39,6 @@ public class PaymentController {
         log.info("*******result:"+result);
         return result;
     }
-    //===服务熔断
-    @GetMapping("/payment/circuit2/{id}")
-    public String paymentCircuitBreaker2(@PathVariable("id") Integer id){
-        String result = paymentService.paymentCircuitBreaker(id);
-        log.info("*******result:"+result);
-        return result;
-    }
-
 
 }
 
